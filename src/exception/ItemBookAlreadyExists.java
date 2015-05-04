d677b23741d7be3feb5bb04e2b4a109d70a49583
pierro42100 <pierrick@wifi-salsa-345.priv.enst-bretagne.fr>
2015-05-04 15:16:07 +0200

    Exceptions

--- src/exception/ItemBookAlreadyExists.java
+++ src/exception/ItemBookAlreadyExists.java
@@ -2,4 +2,7 @@ package exception;
 
 public class ItemBookAlreadyExists extends Exception {
 
+	public ItemBookAlreadyExists(String message) {
+		super(message);
+	}
 }
--- src/exception/ItemFilmAlreadyExists.java
+++ src/exception/ItemFilmAlreadyExists.java
@@ -2,4 +2,8 @@ package exception;
 
 public class ItemFilmAlreadyExists extends Exception {
 
+	public ItemFilmAlreadyExists(String message) {
+		super(message);
+	}
+	
 }
